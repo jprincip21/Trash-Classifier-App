fecb06590cbef842f581e211f2529ab839022395
jprincip21 <jprincip21@gmail.com>
2025-07-25 15:17:46 -0400

    Added logic to load content function to pull List of files instead of individual file

--- lib/views/pages/saved_data_page.dart
+++ lib/views/pages/saved_data_page.dart
@@ -1,7 +1,8 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:trash_classifier_app/utils/app_directory.dart';
 
 class SavedDataPage extends StatefulWidget {
   /// Holds user saved data
@@ -13,26 +14,52 @@ class SavedDataPage extends StatefulWidget {
 
 class _SavedDataPageState extends State<SavedDataPage> {
   //TODO: change from loading singluar file to loading list of files, use show search widget with listview builder
+  //TODO: Probably use a value Notifier to update the page every time a new image is saved.
+  //TODO: Most likely convert the appdirectory to a constant because the same line of code is being constantly repeated.
+  //TODO:
+
   List<File>? loadedFiles;
   File? loadedContent;
 
   @override
   void initState() {
     super.initState();
-    _loadImages();
+    _loadContent();
   }
 
-  Future<void> _loadImages() async {
-    final String appDirectory = (await getApplicationDocumentsDirectory()).path;
+  Future<void> _loadContent() async {
+    final Directory appDirectory = await getAppDirectory();
+    final String appDirectoryPath = appDirectory.path;
 
-    final String fileName = "a.jpg";
+    final Directory userSavedDataDir = Directory(
+      "$appDirectoryPath/user_saved_data",
+    );
 
-    final file = File("$appDirectory/user_saved_data/a/$fileName");
-    if (await file.exists()) {
-      setState(() {
-        loadedContent = file;
-      });
+    final List<FileSystemEntity> userSavedDataContents = await userSavedDataDir
+        .list()
+        .toList();
+
+    for (final entity in userSavedDataContents) {
+      if (entity is Directory) {
+        log('📁 Folder: ${entity.path}');
+        List<FileSystemEntity> folder = await entity.list().toList();
+
+        for (final file in folder) {
+          if (file is File) {
+            log("File: ${file.path}");
+            setState(() {
+              loadedContent = file;
+            });
+          }
+        }
+      }
     }
+    final selectedFile = File("$appDirectoryPath/user_saved_data/a/a.jpg");
+    // if (await selectedFile.exists()) {
+    //   setState(() {
+    //     loadedContent = selectedFile;
+    //   });
+    // }
   }
 
   @override
