169c1fbd5260e9de11c61deffed05b8ca540bc2a
jprincip21 <jprincip21@gmail.com>
2025-08-02 00:10:17 -0400

    Updated TODO

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:trash_classifier_app/app/app.dart';
 
 void main() {
   /// Run App
-  //TODO: Next Steps: Update the saved data page to refresh whenever a new item is added.
-  //TODO: Implement TKlite model of the trash classifier
+  //TODO: Implement TFlite model of the trash classifier
   runApp(const MyApp());
 }
