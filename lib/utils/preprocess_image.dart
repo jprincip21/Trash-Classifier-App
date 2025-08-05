f65e9a51f29c05fd60fbdc7077e7c84c2476f889
jprincip21 <jprincip21@gmail.com>
2025-08-04 23:01:45 -0400

    Updated logic to return preprocessed data in correct shape

--- lib/utils/preprocess_image.dart
+++ lib/utils/preprocess_image.dart
@@ -1,12 +1,13 @@
+import 'dart:developer';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
-import 'package:tflite_flutter/tflite_flutter.dart';
 
-Future<List<double>> preProcessImage(File imagePath) async {
+Future<List<List<List<List<double>>>>> preProcessImage(File imagePath) async {
+  log("PreProcessing Image");
+
   final int imageSize = 224;
   final Uint8List imageBytes = await imagePath.readAsBytes();
-  List<double> imageAsFloatList = [];
 
   img.Image? image = img.decodeImage(imageBytes);
   if (image == null) throw Exception("Could not decode image");
@@ -17,18 +18,37 @@ Future<List<double>> preProcessImage(File imagePath) async {
     height: imageSize,
   );
 
+  List<List<List<double>>> imageAsFloatList = List.generate(
+    imageSize,
+    (index) => List.generate(imageSize, (index) => List.filled(3, 0.0)),
+  );
+
   for (int y = 0; y < imageSize; y++) {
     for (int x = 0; x < imageSize; x++) {
       img.Pixel pixel = resizedImage.getPixel(x, y);
 
-      // Extract and normalize RGB values
+      // Normalize RGB values from 0-255 to 0-1
       double r = pixel.r / 255.0;
       double g = pixel.g / 255.0;
       double b = pixel.b / 255.0;
 
-      imageAsFloatList.addAll([r, g, b]);
+      imageAsFloatList[y][x][0] = r;
+      imageAsFloatList[y][x][1] = g;
+      imageAsFloatList[y][x][2] = b;
     }
   }
-  imageAsFloatList.reshape([1, 224, 224, 3]);
-  return imageAsFloatList;
+  return [
+    imageAsFloatList,
+  ]; //Put imageAsFloatList in a 1D list because expected shape is [1, 224, 224, 3]
+
+  // 1: the number of images being sent at once.
+  // 224: The row of the image (matrix)
+  // 224: The column of the image (matrix)
+  // 3: color chanels each pixel
+
+  //When we specify an index such as [10, 180, 1], we are on the 11th row, 181st column, and looking at the green colour channel
+
+  // the bottom most portion of the list (3) will be the colour values of the pixel where each number specifies the either the red green or blue values for that pixel.
+  // The second bottom most portion of the list (224) is the column (width of the image) we are looking at. For each column there is 224 pixels so well have 224 items of size 3.
+  // the highest portion of the list is the row (height of the image). For each column we will have 1 row of 224 pixels.
 }
