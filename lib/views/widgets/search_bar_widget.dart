bd4ba4157148d7bb7835196707ba6d9b24cbc9f7
jprincip21 <jprincip21@gmail.com>
2025-08-10 19:53:59 -0400

    Created a function which will pull all folder names and send to custom search delegate

--- lib/views/widgets/search_bar_widget.dart
+++ lib/views/widgets/search_bar_widget.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:trash_classifier_app/data/classes/custom_search_delegate.dart';
+import 'package:trash_classifier_app/utils/app_directory.dart';
 
 class SearchBarWidget extends StatefulWidget {
   const SearchBarWidget({super.key});
@@ -9,11 +13,26 @@ class SearchBarWidget extends StatefulWidget {
 }
 
 class _SearchBarWidgetState extends State<SearchBarWidget> {
+  List<String> folderNames = [];
+
+  Future<void> _loadContent() async {
+    final List<Directory> loadedFolders = await loadFolders();
+    folderNames = loadedFolders.map((f) => basename(f.path)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        showSearch(context: context, delegate: CustomSearchDelegate());
+      onPressed: () async {
+        if (!mounted) {
+          return;
+        }
+        await _loadContent();
+        showSearch(
+          context: context,
+          delegate: CustomSearchDelegate(searchTerms: folderNames),
+        );
       },
       icon: Icon(Icons.search),
     );
